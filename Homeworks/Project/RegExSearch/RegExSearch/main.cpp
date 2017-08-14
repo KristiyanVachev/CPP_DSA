149702853e930fa59e7be6298dc77a59170a8437
KristiyanVachev <typhon04@gmail.com>
2017-08-15 01:04:15 +0300

    Added error handling

--- Homeworks/Project/RegExSearch/RegExSearch/main.cpp
+++ Homeworks/Project/RegExSearch/RegExSearch/main.cpp
@@ -3,209 +3,69 @@
 #include <string>
 #include <fstream>
 #include "iostream"
+#include <stdexcept>
+
 
 #include "DoublyLinkedList/DoublyLinkedList.h"
 #include "InputChunk.h"
+
+#include <filesystem>
 
 using namespace std;
 
 DLNode<InputChunk>* Iterate(DoublyLinkedList<InputChunk>* inputChunks, DLNode<InputChunk>* currentNode);
 DLNode<InputChunk>* Concat(DoublyLinkedList<InputChunk>* inputChunks, DLNode<InputChunk>* currentNode);
 DLNode<InputChunk>* Unite(DoublyLinkedList<InputChunk>* inputChunks, DLNode<InputChunk>* currentNode);
+Automata* ConstructAutomata(std::string regEx);
 
 int main(char argc, char* argv[])
 {
 	//string input = "\\s(((a*).b).(\\\\|/))";
 	//string input = "a*.b*.(\\\\|/.c*)";
-	string input = "(((a*).b).(\\\\|/))";
-	string word = "ab\\/";
+	string input = "(((a*).b).\\v.(\\\\|/))";
+	string word = "ab\\";
 
-	DoublyLinkedList<InputChunk>* inputChunks = new DoublyLinkedList<InputChunk>();
+	//TODO check if file or directory
+	//char* path = "D:\\Programming\\CPP_DSA\\Homeworks\\Project\\RegExSearch\\RegExSearch\\main.cpp";
+	//struct stat s;
+	//if (stat(path, &s) == 0)
+	//{
+	//	if (s.st_mode & S_IFDIR)
+	//	{
+	//		cout << "directory" << endl;
+	//	}
+	//	else if (s.st_mode & S_IFREG)
+	//	{
+	//		cout << "file" << endl;
+	//	}
+	//	else
+	//	{
+	//		cout << "else" << endl;
+	//	}
+	//}
+	//else
+	//{
+	//	cout << "error" << endl;
+	//}
 
-	bool isSpecial = false;
-	ChunkType type;
+	//TODO multiple files
+	//std::string directory = "D:\\Programming\\CPP_DSA\\Homeworks\\Project\\RegExSearch\\RegExSearch";
+	//for (auto & p : experimental::filesystem::directory_iterator(directory))
+	//{
+	//	std::cout << p << std::endl;
+	//}
 
-	Stack<DLNode<InputChunk>> openBrackets;
-	LinkedList<InputChunk> closingBrackets;
-
-	for (char letter : input)
+	Automata * automata;
+	try
 	{
-		Specials special = None;
-		Automata* automata = nullptr;
-		DLNode<InputChunk>* bracketPair = nullptr;
-
-		if (isSpecial)
-		{
-			isSpecial = false;
-			type = AutomataChunk;
-
-			switch (letter)
-			{
-			case 's':
-				special = Whitespace;
-				break;
-			case 'd':
-				special = AnyDigit;
-				break;
-			case 'a':
-				special = AnyLetter;
-				break;
-			case 'e':
-				special = Empty;
-				break;
-			case '\\':
-				special = None;
-				type = AutomataChunk;
-				break;
-			default:
-				//TODO destroy everything before returning
-				std::cout << "ERROR: Invalid symbol after \\" << std::endl;
-				return 1;
-			}
-		}
-		else
-		{
-			switch (letter)
-			{
-			case '(':
-				type = OpeningBracket;
-				break;
-			case ')':
-				type = ClosingBracket;
-				bracketPair = openBrackets.Pop();
-				if (bracketPair == nullptr)
-				{
-					std::cout << "ERROR: Closing bracket without an opening one." << std::endl;
-					return 1;
-				}
-				break;
-			case '*':
-				type = Iteration;
-				break;
-			case '.':
-				type = Concatanation;
-				break;
-			case '|':
-				type = Union;
-				break;
-			case '\\':
-				isSpecial = true;
-				continue;
-				break;
-			default:
-				//TODO check if letter is in scope of valid symbols
-				if (!isalpha(letter))
-				{
-					/*std::cout << "ERROR: Invalid symbol." << std::endl;
-					return 1;*/
-				}
-				type = AutomataChunk;
-				break;
-			}
-		}
-
-		if (type == AutomataChunk)
-		{
-			if (inputChunks->Tail() != nullptr && inputChunks->Tail()->Value()->Type() == AutomataChunk)
-			{
-				inputChunks->Tail()->Value()->AutomataChunk()->ConcatState(letter, special);
-				continue;
-			}
-			else
-			{
-				automata = new Automata;
-				automata->ConcatState(letter, special);
-			}
-		}
-
-		InputChunk* chunk = new InputChunk(type, automata, bracketPair);
-
-		if (type == ClosingBracket)
-		{
-			closingBrackets.AddTail(chunk);
-		}
-
-		inputChunks->AddTail(chunk);
-
-		if (type == OpeningBracket)
-		{
-			openBrackets.Add(inputChunks->Tail());
-		}
+		automata = ConstructAutomata(input);
 	}
-
-	//Reduce each closing bracket
-
-	Node<InputChunk>* currentClosingBracket = closingBrackets.Head();
-	bool noBrackets = false;
-
-	while (currentClosingBracket != nullptr || noBrackets == true)
+	catch (const std::invalid_argument& e)
 	{
-		DLNode<InputChunk>* currentNode;
-
-		if (!noBrackets)
-		{
-			currentNode = currentClosingBracket->Value()->BracketPair();
-
-			//If node after opening bracket isn't automata, invalid regex
-
-			if (currentNode->Next()->Value()->Type() == ClosingBracket || currentNode->Next()->Next()->Value()->Type() == ClosingBracket)
-			{
-				std::cout << "ERROR: Invalidly placed brackets." << std::endl;
-				return 1;
-			}
-
-			currentNode = currentNode->Next()->Next();
-
-		}
-		else
-		{
-			currentNode = inputChunks->Head()->Next();
-		}
-
-		do
-		{
-			switch (currentNode->Value()->Type())
-			{
-			case Union:
-				currentNode = Unite(inputChunks, currentNode);
-				break;
-			case Concatanation:
-				currentNode = Concat(inputChunks, currentNode);
-				break;
-			case Iteration:
-				currentNode = Iterate(inputChunks, currentNode);
-				break;
-			default:
-				currentNode = currentNode->Next();
-				break;
-			}
-
-		} while (currentNode != nullptr && currentNode->Value()->Type() != ClosingBracket);
-
-
-		if (!noBrackets)
-		{
-			closingBrackets.Remove(currentClosingBracket);
-			currentClosingBracket = closingBrackets.Head();
-
-			inputChunks->Remove(currentNode->Value()->BracketPair());
-			inputChunks->Remove(currentNode);
-		}
-
-		if (currentClosingBracket == nullptr && noBrackets == false && inputChunks->Head()->Next() != nullptr)
-		{
-			noBrackets = true;
-		}
-		else
-		{
-			noBrackets = false;
-		}
+		std::cout << e.what() << endl;
+		//TODO free other memory
+		return 1;
 	}
-
-	//TODO check if there's only one chunk which is automata
-
-	Automata* automata = inputChunks->Head()->Value()->AutomataChunk();
-	delete inputChunks;
 
 	//Search word
 
@@ -255,6 +115,8 @@ int main(char argc, char* argv[])
 			break;;
 		}
 	}
+
+	cout << word << " is recognied: " << isRecognized << endl;
 
 	return 0;
 }
@@ -350,3 +212,191 @@ DLNode<InputChunk>* Unite(DoublyLinkedList<InputChunk>* inputChunks, DLNode<Inpu
 	return currentNode;
 }
 
+Automata* ConstructAutomata(std::string regEx)
+{
+	DoublyLinkedList<InputChunk>* inputChunks = new DoublyLinkedList<InputChunk>();
+
+	bool isSpecial = false;
+	ChunkType type;
+
+	Stack<DLNode<InputChunk>> openBrackets;
+	LinkedList<InputChunk> closingBrackets;
+
+	for (char letter : regEx)
+	{
+		Specials special = None;
+		Automata* automata = nullptr;
+		DLNode<InputChunk>* bracketPair = nullptr;
+
+		if (isSpecial)
+		{
+			isSpecial = false;
+			type = AutomataChunk;
+
+			switch (letter)
+			{
+			case 's':
+				special = Whitespace;
+				break;
+			case 'd':
+				special = AnyDigit;
+				break;
+			case 'a':
+				special = AnyLetter;
+				break;
+			case 'e':
+				special = Empty;
+				break;
+			case '\\':
+				special = None;
+				type = AutomataChunk;
+				break;
+			default:
+				//TODO destroy everything before returning
+				throw std::invalid_argument("Invalid symbol after \\");
+			}
+		}
+		else
+		{
+			switch (letter)
+			{
+			case '(':
+				type = OpeningBracket;
+				break;
+			case ')':
+				type = ClosingBracket;
+				bracketPair = openBrackets.Pop();
+				if (bracketPair == nullptr)
+				{
+					throw std::invalid_argument("Closing bracket without an opening one.");
+				}
+				break;
+			case '*':
+				type = Iteration;
+				break;
+			case '.':
+				type = Concatanation;
+				break;
+			case '|':
+				type = Union;
+				break;
+			case '\\':
+				isSpecial = true;
+				continue;
+				break;
+			default:
+				//TODO check if letter is in scope of valid symbols
+				if (!isalpha(letter))
+				{
+					/*std::cout << "ERROR: Invalid symbol." << std::endl;
+					return 1;*/
+				}
+				type = AutomataChunk;
+				break;
+			}
+		}
+
+		if (type == AutomataChunk)
+		{
+			if (inputChunks->Tail() != nullptr && inputChunks->Tail()->Value()->Type() == AutomataChunk)
+			{
+				inputChunks->Tail()->Value()->AutomataChunk()->ConcatState(letter, special);
+				continue;
+			}
+			else
+			{
+				automata = new Automata;
+				automata->ConcatState(letter, special);
+			}
+		}
+
+		InputChunk* chunk = new InputChunk(type, automata, bracketPair);
+
+		if (type == ClosingBracket)
+		{
+			closingBrackets.AddTail(chunk);
+		}
+
+		inputChunks->AddTail(chunk);
+
+		if (type == OpeningBracket)
+		{
+			openBrackets.Add(inputChunks->Tail());
+		}
+	}
+
+	//Reduce each closing bracket
+
+	Node<InputChunk>* currentClosingBracket = closingBrackets.Head();
+	bool noBrackets = false;
+
+	while (currentClosingBracket != nullptr || noBrackets == true)
+	{
+		DLNode<InputChunk>* currentNode;
+
+		if (!noBrackets)
+		{
+			currentNode = currentClosingBracket->Value()->BracketPair();
+
+			//If node after opening bracket isn't automata, invalid regex
+
+			if (currentNode->Next()->Value()->Type() == ClosingBracket || currentNode->Next()->Next()->Value()->Type() == ClosingBracket)
+			{
+				throw std::invalid_argument("Invalidly placed brackets.");
+			}
+
+			currentNode = currentNode->Next()->Next();
+
+		}
+		else
+		{
+			currentNode = inputChunks->Head()->Next();
+		}
+
+		do
+		{
+			switch (currentNode->Value()->Type())
+			{
+			case Union:
+				currentNode = Unite(inputChunks, currentNode);
+				break;
+			case Concatanation:
+				currentNode = Concat(inputChunks, currentNode);
+				break;
+			case Iteration:
+				currentNode = Iterate(inputChunks, currentNode);
+				break;
+			default:
+				currentNode = currentNode->Next();
+				break;
+			}
+
+		} while (currentNode != nullptr && currentNode->Value()->Type() != ClosingBracket);
+
+
+		if (!noBrackets)
+		{
+			closingBrackets.Remove(currentClosingBracket);
+			currentClosingBracket = closingBrackets.Head();
+
+			inputChunks->Remove(currentNode->Value()->BracketPair());
+			inputChunks->Remove(currentNode);
+		}
+
+		if (currentClosingBracket == nullptr && noBrackets == false && inputChunks->Head()->Next() != nullptr)
+		{
+			noBrackets = true;
+		}
+		else
+		{
+			noBrackets = false;
+		}
+	}
+
+	//TODO check if there's only one chunk which is automata
+
+	Automata* automata = inputChunks->Head()->Value()->AutomataChunk();
+	delete inputChunks;
+
+	return automata;
+}
